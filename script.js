8bbe7effbe9c4006d39b6abfcb07721cd189eabf
Pedriniamanda123 <146124413+Pedriniamanda123@users.noreply.github.com>
2025-04-07 09:11:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ botaoDeAcessibilidade.addEventListener('click', function (){
     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
    })
+   const alternaContraste = document.getElementById('alterna-contraste')
+   alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste')
+})
+const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
